64523396f0945248ccd298ee9dcadbc16ec6d87b
jscblack <jscblack@163.com>
2020-03-17 10:00:06 +0800

    upd

---
+++ test.c
@@ -0,0 +1,20 @@
+/*
+ * @Author: Gehrychiang
+ * @LastEditTime: 2020-03-17 09:54:03
+ * @Website: www.yilantingfeng.site
+ * @E-mail: gehrychiang@aliyun.com
+ */
+#include <stdio.h>
+#include <wiringPi.h>
+#include <ads1115.h>
+#define LED 2
+int main()
+{
+    wiringPiSetupGpio();
+    pinMode(LED,OUTPUT);
+    digitalWrite(LED,1);
+    delay(10000);
+    digitalWrite(LED,0);
+    pinMode(LED,INPUT);
+    return 0;
+}
